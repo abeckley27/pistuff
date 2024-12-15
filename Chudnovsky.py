09ac9fb8b2c68c155c1427151aab5339749c6316
abeckley27 <aidanbeckley@gmail.com>
2024-12-15 14:33:48 -0500

    replace python code with the correct version

--- Chudnovsky.py
+++ Chudnovsky.py
@@ -6,60 +6,43 @@ Created on Sat Dec 14 01:51:23 2024
 """
 
 import time
-import decimal as dec
-
-dec.getcontext().prec = 1000
-a = 545140134
-b = 13591409
-c = 640320
-
-def factorial(n):
-    output = dec.Decimal(1)
-    for k in range(2, n + 1):
-        output *= k
-        
-    return output
-
-
-def compute_term(k):
-    sign = (k % 2) * -2 + 1
-    
-    #For the time being, just calculate each factorial separately
-    k_fact = factorial(k)
-    three_k_fact = factorial(3*k)
-    six_k_fact = factorial(6*k)
-
-    linear_term = dec.Decimal(a) * k + b
-    m = dec.Decimal(c)**dec.Decimal(3*k + 1.5)
-    
-    
-    output = six_k_fact / three_k_fact
-    output = output * linear_term / (k_fact**3)
-    output = sign * output / m
-    return output
-
+import decimal
 
 t0 = time.time()
-series = 0
-N = 80
+decimal.getcontext().prec = 100000
+a = decimal.Decimal(545140134)
+b = decimal.Decimal(13591409)
+c = decimal.Decimal(640320)
 
-zeroth_term = compute_term(0)
+
+def binary_split(a, b):
+    if b == a + 1:
+        Pab = -(6*a - 5)*(2*a - 1)*(6*a - 1)
+        Qab = 10939058860032000 * a**3
+        Rab = Pab * (545140134*a + 13591409)
+    else:
+        m = (a + b) // 2
+        Pam, Qam, Ram = binary_split(a, m)
+        Pmb, Qmb, Rmb = binary_split(m, b)
+        
+        Pab = Pam * Pmb
+        Qab = Qam * Qmb
+        Rab = Qmb * Ram + Pam * Rmb
+    return Pab, Qab, Rab
 
 
+def chudnovsky(n):
+    P1n, Q1n, R1n = binary_split(1, n)
+    return (426880 * decimal.Decimal(10005).sqrt() * Q1n) / (13591409*Q1n + R1n)
 
-for k in range(N):
-    series += compute_term(k)
+pi = chudnovsky(5000)
 
-series *= 12
-pi = dec.Decimal(1) / series
-
-print(pi)
+#print(pi)
 print("Time: %.3f" %(time.time() - t0))
-
 print("Checking digits")
 
 
-f = open("Pi-reference.txt", 'r')
+f = open("Pi-Dec-100k.txt", 'r')
 fstr = f.read()
 f.close()
 
